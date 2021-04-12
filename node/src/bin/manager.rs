f9c857cefa8850e2f9cc90bed22462cb83dd9368
David Lutterkort <lutter@watzmann.net>
2021-04-12 13:34:58 -0700

    node: Add `graphman rewind`

--- node/src/bin/manager.rs
+++ node/src/bin/manager.rs
@@ -103,6 +103,11 @@ pub enum Command {
         /// The id of the deployment to unassign
         id: String,
     },
+    Rewind {
+        id: String,
+        block_hash: String,
+        block_number: i32,
+    },
     /// Check and interrogate the configuration
     ///
     /// Print information about a configuration file without
@@ -294,6 +299,14 @@ async fn main() {
         Reassign { id, node } => {
             let store = make_store();
             commands::assign::reassign(store, id, node)
+        }
+        Rewind {
+            id,
+            block_hash,
+            block_number,
+        } => {
+            let store = make_store();
+            commands::rewind::run(store, id, block_hash, block_number)
         }
         Listen(cmd) => {
             use ListenCommand::*;
